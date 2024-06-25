f2fe3116457143c16eb78fe9ce5ff15b325df3e1
Aline <aline_lima@outlook.com>
2024-06-25 10:19:35 -0300

    Criação rota GET

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from models.task import Task
 
 app = Flask(__name__)
-
-# CRUD
 
 tasks = []
 task_id_control = 1
@@ -20,8 +18,21 @@ def create_task():
     )
     task_id_control += 1
     tasks.append(new_task)
-    print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    message = {"message": "Nova tarefa criada com sucesso", "id": new_task.id}
+    return jsonify(message)
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict())
+
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+    return jsonify(output)
 
 
 if __name__ == "__main__":
