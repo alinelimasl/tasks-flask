72d541f5201a19d9e9a222d19591ac029c8e3afa
Aline <aline_lima@outlook.com>
2024-06-25 16:51:08 -0300

    Criação rota PUT e DELETE

--- app.py
+++ app.py
@@ -22,26 +22,47 @@ def create_task():
     return jsonify(message)
 
 
-@app.route('/tasks', methods=['GET'])
+@app.route("/tasks", methods=["GET"])
 def get_tasks():
     task_list = []
     for task in tasks:
         task_list.append(task.to_dict())
 
-    output = {
-        "tasks": task_list,
-        "total_tasks": len(task_list)
-    }
+    output = {"tasks": task_list, "total_tasks": len(task_list)}
     return jsonify(output)
 
 
-@app.route('/tasks/<int:id>', methods=['GET'])
-def get_task(id):
+@app.route("/tasks/<int:id>", methods=["PUT"])
+def update_task(id):
+    task = None
     for t in tasks:
         if t.id == id:
-            return jsonify(t.to_dict())
+            task = t
+    if task is None:
+        return jsonify(
+            {"message": "Não foi possível encontrar a atividade"}), 404
 
-    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+    data = request.get_json()
+    task.title = data["title"]
+    task.description = data["description"]
+    task.completed = data["completed"]
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+        if not task:
+            return jsonify(
+                {"message": "Não foi possível encontrar a atividade"}), 404
+
+        tasks.remove(task)
+        return jsonify({"message": "Tarefa deletada com sucesso"})
 
 
 if __name__ == "__main__":
